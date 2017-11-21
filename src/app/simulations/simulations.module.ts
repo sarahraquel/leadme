94b1ed4906a962426197a700a75d4bd68728779b
Sarah <sarahraqueld@gmail.com>
2017-11-21 19:25:48 -0300

    Add simulations' components

--- src/app/simulations/simulations.module.ts
+++ src/app/simulations/simulations.module.ts
@@ -3,6 +3,8 @@ import { SimulationsComponent } from './simulations.component';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NextComponent } from './next/next.component';
+import { PreviousComponent } from './previous/previous.component';
 
 
 @NgModule({
@@ -11,7 +13,7 @@ import { CommonModule } from '@angular/common';
     FormsModule,
     SimulationsRoutingModule,
   ],
-  declarations: [SimulationsComponent]
+  declarations: [SimulationsComponent, NextComponent, PreviousComponent]
 })
 
 export class SimulationsModule { }
