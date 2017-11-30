5b625e7034ae50b0013f0405baaaf737af7d3148
Sarah <sarahraqueld@gmail.com>
2017-11-30 16:38:11 -0300

    Add mais disciplinas

--- src/app/_services/disciplina.service.ts
+++ src/app/_services/disciplina.service.ts
@@ -7,10 +7,14 @@ import { Injectable } from '@angular/core';
 export class DisciplinaService {
 
     disciplinas: Disciplina [] = [
-        new Disciplina("IMD00000", "FUNDAMENTOS MATEMÁTICOS DA COMPUTAÇÃO I", 90, "Obrigatória"),
-        new Disciplina("IMD00000", "CÁLCULO DIFERENCIAL E INTEGRAL I", 90, "Obrigatória"),
-        new Disciplina("IMD00000", "FUNDAMENTOS MATEMÁTICOS DA COMPUTAÇÃO II", 90, "Obrigatória"),
-        new Disciplina("IMD00000", "FUNDAMENTOS MATEMÁTICOS DA COMPUTAÇÃO II", 90, "Optativa")
+        new Disciplina("IMD0028", "Fundamentos Matemáticos da Computação I", 90, "Obrigatória"),
+        new Disciplina("IMD00000", "Cálculo Diferencial e Integral I", 90, "Obrigatória"),
+        new Disciplina("IMD00000", "Fundamentos Matemáticos da Computação II", 90, "Obrigatória"),
+        new Disciplina("IMD00000", "Álgebra Linear", 60, "Optativa"),
+        new Disciplina("DIM0504", "Análise e Projeto Orientado a Objeto", 60, "Optativa"),
+        new Disciplina("IMD0043", "Redes de Computadores", 60, "Optativa"),
+        new Disciplina("IMD00000", "Álgebra Linear", 60, "Optativa"),
+        new Disciplina("IMD0034", "Vetores e Geometria Analítica", 60, "Optativa"),
     ]
 
     disciplinasSelecionadas: Disciplina[] = [
